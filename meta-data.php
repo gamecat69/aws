a222b6f77e91fc74e5465eb4df6e1da29810eb78
gamecat69 <nikansell00@gmail.com>
2017-01-30 18:32:18 +0400

    Update meta-data.php

--- meta-data.php
+++ meta-data.php
@@ -1,13 +1,41 @@
 <html>
 <head>
 <title>EC2 Instance Meta-Data</title>
-
 <style>
+
+.tabler {
+  margin-top: 20px;
+  margin-bottom: 40px;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+.tabler,
+.tabler th,
+.tabler td {
+  border: none;
+  text-align: left;
+  padding: 8px;
+}
+
+.tabler tbody tr:nth-child(even) {
+  background-color: #f2f2f2
+}
+
+.table-header {
+	background-color:#4CAF50;
+	color:white;
+}
+
+tr:nth-child(even) {
+	background-color: #f2f2f2
+}
 
 body {
 	text-align: left;
 	color: #281B18;
-	background: #F3EEEC;
+	background: #FFFFFF;
+	font-family: Arial, "Trebuchet MS", Helvetica, sans-serif;
 }
 
 h1 {
@@ -16,33 +44,19 @@ h1 {
 	font-weight: bold;
 }
 
-.title {
-	font: 0.8em 'Arial Rounded MT Bold';
-	font-weight: bold;
-}
-
-.dots {
-	font: 0.8em 'Arial Rounded MT Bold';
-	font-weight: bold;
-	color: #DA3A15;
-}
-
-.info {
-	font-weight: normal;
-	font: 0.8em 'Arial';
-}
-
 </style>
-
 <head>
 <body>
-
 <h1>EC2 Instance Meta-Data</h1>
+
+<table class="tabler">
+<tr class="table-header">
+<th>Property</th><th>Value</th>
+</tr>
 
 <?php
 
 // Create array
-
 $info=array("ami-id",
 "ami-launch-index",
 "ami-manifest-path",
@@ -65,10 +79,11 @@ for ($x=0; $x<$arrlength; $x++)
 {
 	$path = "http://169.254.169.254/latest/meta-data/" . $info[$x]; 
 	$data = file_get_contents($path);
-	echo "<span class=\"title\">", $info[$x], "</span><span class=\"dots\"> : </span><span class=\"info\">",$data, "</span><br/>\n";
+	//echo "<span class=\"title\">", $info[$x], "</span><span class=\"dots\"> : </span><span class=\"info\">",$data, "</span><br/>\n";
+	echo "<tr><th>" , $info[$x] , "</th><th>" , $data "</th></tr>\n";
 }
 
 ?>
-
+</table>
 </body>
 </html>
